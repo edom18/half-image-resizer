455833a8ff6d18b632d088f18db830fe34e559e7
edom18 <edo.m18@gmail.com>
2016-05-24 11:49:02 +0900

    ロジックを整理

--- main.js
+++ main.js
@@ -2,53 +2,86 @@
 
     'use strict';
 
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
-    document.body.appendChild(canvas);
-
     var dropZone = document.getElementById('dropZone');
-    dropZone.addEventListener('mouseenter', function (evt) {
-        console.log('enter');
-    }, false);
     dropZone.addEventListener('dragover', function (evt) {
         evt.preventDefault();
     }, false);
-
     dropZone.addEventListener('drop', function (evt) {
         evt.preventDefault();
         console.log('drop');
 
         if (evt.dataTransfer.files.length > 0) {
-            loadFile(evt.dataTransfer.files[0]);
+            loadImage(evt.dataTransfer.files[0]).then(loadedImage);
         }
     }, false);
 
-    function loadFile(file) {
-        var reader = new FileReader();
-        reader.onload = function (evt) {
-            var img = new Image();
-            img.onload = function () {
-                canvas.width  = this.width/2;
-                canvas.height = this.height/2;
+    /**
+     * 画像読み込み後のハンドラ
+     */
+    function loadedImage(image) {
+        var width  = image.width/2;
+        var height = image.height/2;
 
-                context.drawImage(this, 0, 0, this.width/2, this.height/2);
+        var buffer1  = document.createElement('canvas');
+        var context = buffer1.getContext('2d');
 
-                var cv = sharpness(canvas);
-                document.body.appendChild(cv);
+        buffer1.width  = width;
+        buffer1.height = height;
 
-                cv.toBlob(function (blob) {
-                    var a = document.createElement('a');
-                    a.download = 'resized.png';
-                    a.href = URL.createObjectURL(blob);
+        context.drawImage(image, 0, 0, width, height);
 
-                    var evt = document.createEvent('MouseEvent');
-                    evt.initEvent('click', true, false);
-                    a.dispatchEvent(evt);
-                });
-            };
-            img.src = this.result;
+        var buffer2 = sharpness(buffer1);
+
+        buffer2.toBlob(function (blob) {
+            download(blob);
+        });
+
+        view(buffer1, document.body);
+        view(buffer2, document.body);
+    }
+
+    /**
+     * Canvasの内容をimg要素として表示する
+     */
+    function view(canvas, target) {
+        var url = canvas.toDataURL();
+        var img = new Image();
+        img.onload = function () {
+            target.appendChild(this);
         };
-        reader.readAsDataURL(file);
+        img.src = url;
+    }
+
+    /**
+     * ダウンロードを実行
+     */
+    function download(blob) {
+        var a = document.createElement('a');
+        a.download = 'resized.png';
+        a.href = URL.createObjectURL(blob);
+
+        var evt = document.createEvent('MouseEvent');
+        evt.initEvent('click', true, false);
+        a.dispatchEvent(evt);
+    }
+
+    /**
+     * ファイルをロード
+     *
+     * @param {File} file
+     */
+    function loadImage(file) {
+        return new Promise(function (resolve, reject) {
+            var reader = new FileReader();
+            reader.onload = function (evt) {
+                var img = new Image();
+                img.onload = function () {
+                    resolve(img);
+                };
+                img.src = this.result;
+            };
+            reader.readAsDataURL(file);
+        });
     }
 
 }(window));
